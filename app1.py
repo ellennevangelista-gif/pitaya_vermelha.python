ffc75307a318864c988b2f231c438843f51a8e06
ellenevangelista-gif <ellennevangelista@gmail.com>
2026-09-23 09:31:47 -0300

    Ok

---
+++ app1.py
@@ -0,0 +1,28 @@
+# Esse aplicativo serve para alguma coisa
+
+'''
+Isso é um bloco de comentários, 
+posso ter várias linhas,
+o Python ignora tudo isso
+independentementes da quantidade de linhas.
+''' 
+
+# Cria as variáveis para receber os dados do usuário
+
+#print ("Estou com fome!")
+
+#name = "Joca da Silva"
+#age = 43 
+
+print ()
+name = input ("Qual o seu nome?")
+age = input ("Quantos anos você tem?")
+
+print ()
+print ("Olá", name, "!") 
+print ()    
+print ("Você tem" , age, "anos")
+print ()
+
+
+
