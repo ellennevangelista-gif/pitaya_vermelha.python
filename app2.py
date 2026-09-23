cf4046f48a454202c025757cbb1d8c2d086acdff
ellenevangelista-gif <ellennevangelista@gmail.com>
2026-09-23 10:18:04 -0300

    Ok

---
+++ app2.py
@@ -0,0 +1,12 @@
+name = input ("Qual é o nome?") 
+age = input ("Qual  sua idade?")
+
+print(type(name))
+print(type(age))
+
+age = int(age)
+
+older = age + 19 
+print(f"{name} terá {older} daqui a 18 anos.")
+
+
